0550e10d61fa1f0c2bf01511478e844763996e54
Barsha2001 <126144169+Barsha2001@users.noreply.github.com>
2023-09-01 20:27:06 +0530

    Add files via upload

---
+++ pythonAssignment.py
@@ -0,0 +1,12 @@
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+
+
+floor_division_result = a // b
+print(f"Floor Division of a and b: {floor_division_result}")
+
+power_result = a ** b
+print(f"a raised to the power b: {power_result}")
+
+equal_values = a == b
+print(f"Are a and b equal?: {equal_values}")
